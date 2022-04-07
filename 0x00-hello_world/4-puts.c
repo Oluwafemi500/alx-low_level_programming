f7cc21d72c9475cdbf1a8430730793a7e11a4c4a
erota oluwafemi <erotaoluwafemi3@gmail.com>
2022-04-07 16:53:14 -0700

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - entry point
  *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
