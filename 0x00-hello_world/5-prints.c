0b1c990c01b809fee05ddc83d2c25ebf75f0b6c7
erota oluwafemi <erotaoluwafemi3@gmail.com>
2022-04-07 17:38:56 -0700

    my first commit

--- 0x00-hello_world/5-prints.c
+++ 0x00-hello_world/5-prints.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  *main - Entry point
  *My hello, printf task
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-printf("with proper grammar , but the outcome is a piece of art.\n");
+printf("with proper grammr, but the outcome is a piece of art,\n");
 return (0);
 }
